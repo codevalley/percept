e0888ba72f9e4dab8d0e88df5158c18aa561e3b7
Narayan Babu <codevalley@live.com>
2024-07-01 15:53:29 +0530

    Added success section

--- webapp/src/components/CreateView.vue
+++ webapp/src/components/CreateView.vue
@@ -12,145 +12,177 @@
         </div>
       </div>
 
-      <!-- Question Input Section -->
-      <div class="bg-neutral-400 rounded-[25px] p-7 mb-12 relative">
-        <div class="relative flex items-center mb-6">
-          <inline-svg src="assets/question-icon.svg" class="text-primary"/>
-          <input 
-            v-model="newQuestion.text" 
-            @keyup.enter="addQuestion"
-            @focus="inputFocused = true"
-            @blur="inputFocused = false"
-            class="w-full bg-transparent text-[34px] font-bold text-primary focus:outline-none"
-            :placeholder="inputFocused ? '' : 'Enter your question here'"
-            :disabled="isSubmitted"
-          >
-          <span v-if="!newQuestion.text && !inputFocused" class="absolute right-0 top-1/2 transform -translate-y-1/2 text-neutral-300 text-[34px] font-bold animate-blink">_</span>
-        </div>
-        <div class="flex justify-between items-center">
-          <div class="flex space-x-5">
-            <!-- Scale button -->
-            <button 
-              @click="setQuestionType('scale')"
-              :class="['w-28 h-10 rounded-full flex items-center justify-center space-x-2 border border-neutral-100', 
-                      newQuestion.response_type === 'scale' ? 'bg-primary text-white' : 'bg-white text-primary']"
+      <template v-if="!isPublished">
+        <!-- Question Input Section -->
+        <div class="bg-neutral-400 rounded-[25px] p-7 mb-12 relative">
+          <div class="relative flex items-center mb-6">
+            <inline-svg src="assets/question-icon.svg" class="text-primary"/>
+            <input 
+              v-model="newQuestion.text" 
+              @keyup.enter="addQuestion"
+              @focus="inputFocused = true"
+              @blur="inputFocused = false"
+              class="w-full bg-transparent text-[34px] font-bold text-primary focus:outline-none"
+              :placeholder="inputFocused ? '' : 'Enter your question here'"
               :disabled="isSubmitted"
             >
-              <inline-svg src="assets/scale-icon.svg" :class="newQuestion.response_type === 'scale' ? 'text-white' : 'text-primary'" 
-                          class="w-6 h-6"
-                          :key="newQuestion.response_type === 'scale' ? 'scale' : 'default'" />
-              <span class="text-base font-medium">Scale</span>
-            </button>
+            <span v-if="!newQuestion.text && !inputFocused" class="absolute right-0 top-1/2 transform -translate-y-1/2 text-neutral-300 text-[34px] font-bold animate-blink">_</span>
+          </div>
+          <div class="flex justify-between items-center">
+            <div class="flex space-x-5">
+              <!-- Scale button -->
+              <button 
+                @click="setQuestionType('scale')"
+                :class="['w-28 h-10 rounded-full flex items-center justify-center space-x-2 border border-neutral-100', 
+                        newQuestion.response_type === 'scale' ? 'bg-primary text-white' : 'bg-white text-primary']"
+                :disabled="isSubmitted"
+              >
+                <inline-svg src="assets/scale-icon.svg" :class="newQuestion.response_type === 'scale' ? 'text-white' : 'text-primary'" 
+                            class="w-6 h-6"
+                            :key="newQuestion.response_type === 'scale' ? 'scale' : 'default'" />
+                <span class="text-base font-medium">Scale</span>
+              </button>
 
-            <!-- Yes/No button -->
+              <!-- Yes/No button -->
+              <button 
+                @click="setQuestionType('boolean')"
+                :class="['w-28 h-10 rounded-full flex items-center justify-center space-x-2 border border-neutral-100', 
+                        newQuestion.response_type === 'boolean' ? 'bg-primary text-white' : 'bg-white text-primary']"
+                :disabled="isSubmitted"
+              >
+                <inline-svg src="assets/yes-no-icon.svg"  
+                            :class="newQuestion.response_type === 'boolean' ? 'text-white' : 'text-primary'"
+                            class="w-6 h-6"
+                            :key="newQuestion.response_type === 'boolean' ? 'boolean' : 'default'"
+                            />
+                <span class="text-base font-medium">Yes/No</span>
+              </button>
+            </div>
+
+            <!-- Add button -->
             <button 
-              @click="setQuestionType('boolean')"
-              :class="['w-28 h-10 rounded-full flex items-center justify-center space-x-2 border border-neutral-100', 
-                      newQuestion.response_type === 'boolean' ? 'bg-primary text-white' : 'bg-white text-primary']"
-              :disabled="isSubmitted"
+              @click="addQuestion"
+              :disabled="!newQuestion.text.trim() || isSubmitted"
+              :class="[
+                'w-28 h-10 rounded-full flex items-center justify-center space-x-2 border',
+                !newQuestion.text.trim() || isSubmitted 
+                  ? 'bg-gray-100 disabled text-neutral-300 border-neutral-100' 
+                  : 'bg-accent-green text-white border-accent-green'
+              ]"
             >
-              <inline-svg src="assets/yes-no-icon.svg"  
-                          :class="newQuestion.response_type === 'boolean' ? 'text-white' : 'text-primary'"
-                          class="w-6 h-6"
-                          :key="newQuestion.response_type === 'boolean' ? 'boolean' : 'default'"
-                          />
-              <span class="text-base font-medium">Yes/No</span>
+              <span class="text-base font-medium">Add</span>
             </button>
           </div>
+        </div>
 
-          <!-- Add button -->
+        <!-- Posted Questions -->
+        <div v-if="questions.length > 0" class="bg-neutral-500 rounded-[25px] p-7 space-y-6 mb-12">
+          <div v-for="(question, index) in questions" :key="question.id" class="flex items-center justify-between">
+            <div class="flex items-center space-x-4">
+              <inline-svg :src="question.response_type === 'scale' ? 'assets/scale-icon.svg' : 'assets/yes-no-icon.svg'"  
+                          :class="creatorAnswers[index] !== null ? 'text-primary' : 'text-neutral-300'" class="w-6 h-6"/>
+              <span :class="['text-2xl font-bold', creatorAnswers[index] !== null ? 'text-primary' : 'text-neutral-300']">
+                {{ question.text }}
+              </span>
+            </div>
+            <div v-if="question.response_type === 'scale'" class="flex space-x-3">
+              <button 
+                v-for="n in 5" 
+                :key="n"
+                @click="selectAnswer(index, n)"
+                :class="['w-[30px] h-[30px] rounded-full border flex items-center justify-center', 
+                        n <= creatorAnswers[index] ? 'bg-primary border-primary' : 'bg-neutral-200 border-neutral-200']"
+                :disabled="isSubmitted"
+              >
+                <div :class="['w-[18px] h-[18px] rounded-full', n <= creatorAnswers[index] ? 'bg-white' : 'bg-transparent']"></div>
+              </button>
+            </div>
+            <div v-else class="flex space-x-3">
+              <button 
+                @click="selectAnswer(index, true)"
+                :class="['w-[30px] h-[30px] rounded-full border flex items-center justify-center', 
+                        creatorAnswers[index] === true ? 'bg-primary border-primary' : 'bg-neutral-200 border-neutral-200']"
+                :disabled="isSubmitted"
+              >
+                <inline-svg 
+                  src="assets/yes-icon.svg" 
+                  :class="[
+                    'w-4 h-4', 
+                    creatorAnswers[index] === true ? 'text-white' : 'text-neutral-300'
+                  ]" 
+                />
+              </button>
+              <button 
+                @click="selectAnswer(index, false)"
+                :class="['w-[30px] h-[30px] rounded-full border flex items-center justify-center', 
+                        creatorAnswers[index] === false ? 'bg-primary border-primary' : 'bg-neutral-200 border-neutral-200']"
+                :disabled="isSubmitted"
+              >
+                <inline-svg 
+                  src="assets/no-icon.svg" 
+                  :class="[
+                    'w-4 h-4', 
+                    creatorAnswers[index] === false ? 'text-white' : 'text-neutral-300'
+                  ]" 
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <!-- Publish Button -->
+        <div class="text-left">
           <button 
-            @click="addQuestion"
-            :disabled="!newQuestion.text.trim() || isSubmitted"
+            @click="finishSurvey"
+            :disabled="!allQuestionsAnswered || isLoading || isSubmitted"
             :class="[
-              'w-28 h-10 rounded-full flex items-center justify-center space-x-2 border',
-              !newQuestion.text.trim() || isSubmitted 
-                ? 'bg-gray-100 disabled text-neutral-300 border-neutral-100' 
-                : 'bg-accent-green text-white border-accent-green'
+              'w-[152px] h-[56px] rounded-full text-center text-2xl font-bold leading-9',
+              !allQuestionsAnswered || isLoading || isSubmitted
+                ? 'bg-gray-100 text-neutral-300'
+                : 'bg-accent-green text-white'
             ]"
           >
-            <span class="text-base font-medium">Add</span>
+            Publish
           </button>
         </div>
-      </div>
 
-      <!-- Posted Questions -->
-      <div v-if="questions.length > 0" class="bg-neutral-500 rounded-[25px] p-7 space-y-6 mb-12">
-        <div v-for="(question, index) in questions" :key="question.id" class="flex items-center justify-between">
+        <!-- Alert Box -->
+        <div v-if="!allQuestionsAnswered" class="flex items-center space-x-2 mb-6 text-accent" style="padding-top: 20px;">
+          <inline-svg src="assets/info-icon.svg" class="text-accent w-5 h-5"/>
+          <span class="text-sm font-medium">Complete self review before publishing</span>
+        </div>
+      </template>
+
+      <!-- Published Section -->
+      <div v-if="isPublished" class="bg-[#D8F89D] rounded-[25px] p-7 mb-12">
+        <div class="w-full text-sky-950 text-2xl font-bold font-['IBM Plex Sans'] leading-9 mb-6 text-left">
+          The review is published and live!
+        </div>
+        
+        <div class="flex items-center justify-start space-x-4">
           <div class="flex items-center space-x-4">
-            <inline-svg :src="question.response_type === 'scale' ? 'assets/scale-icon.svg' : 'assets/yes-no-icon.svg'"  
-                        :class="creatorAnswers[index] !== null ? 'text-primary' : 'text-neutral-300'" class="w-6 h-6"/>
-            <span :class="['text-2xl font-bold', creatorAnswers[index] !== null ? 'text-primary' : 'text-neutral-300']">
-              {{ question.text }}
-            </span>
-          </div>
-          <div v-if="question.response_type === 'scale'" class="flex space-x-3">
+            <span class="text-primary text-lg font-normal leading-7 whitespace-nowrap">Review code</span>
             <button 
-              v-for="n in 5" 
-              :key="n"
-              @click="selectAnswer(index, n)"
-              :class="['w-[30px] h-[30px] rounded-full border flex items-center justify-center', 
-                      n <= creatorAnswers[index] ? 'bg-primary border-primary' : 'bg-neutral-200 border-neutral-200']"
-              :disabled="isSubmitted"
+              class="h-10 bg-accent-green text-white rounded-full flex items-center justify-between px-4 border border-accent-green"
+              @click="copyToClipboard(surveyCode)"
             >
-              <div :class="['w-[18px] h-[18px] rounded-full', n <= creatorAnswers[index] ? 'bg-white' : 'bg-transparent']"></div>
+              <span class="text-base font-medium mr-2">{{ surveyCode }}</span>
+              <inline-svg src="assets/copy-icon.svg" class="w-5 h-5 text-white" />
             </button>
           </div>
           
-          
-          <div v-else class="flex space-x-3">
+          <div class="flex items-center space-x-4">
+            <span class="text-primary text-lg font-normal leading-7 whitespace-nowrap">Review Link</span>
             <button 
-              @click="selectAnswer(index, true)"
-              :class="['w-[30px] h-[30px] rounded-full border flex items-center justify-center', 
-                      creatorAnswers[index] === true ? 'bg-primary border-primary' : 'bg-neutral-200 border-neutral-200']"
-              :disabled="isSubmitted"
+              class="h-10 bg-accent-green text-white rounded-full flex items-center justify-between px-4 border border-accent-green"
+              @click="copyToClipboard(surveyLink)"
             >
-              <inline-svg 
-                src="assets/yes-icon.svg" 
-                :class="[
-                  'w-4 h-4', 
-                  creatorAnswers[index] === true ? 'text-white' : 'text-neutral-300'
-                ]" 
-              />
-            </button>
-            <button 
-              @click="selectAnswer(index, false)"
-              :class="['w-[30px] h-[30px] rounded-full border flex items-center justify-center', 
-                      creatorAnswers[index] === false ? 'bg-primary border-primary' : 'bg-neutral-200 border-neutral-200']"
-              :disabled="isSubmitted"
-            >
-              <inline-svg 
-                src="assets/no-icon.svg" 
-                :class="[
-                  'w-4 h-4', 
-                  creatorAnswers[index] === false ? 'text-white' : 'text-neutral-300'
-                ]" 
-              />
+              <span class="text-base font-medium mr-2">Copy URL</span>
+              <inline-svg src="assets/copy-icon.svg" class="w-5 h-5 text-white" />
             </button>
           </div>
         </div>
-      </div>
 
-      <!-- Publish Button -->
-      <div class="text-left">
-        <button 
-          @click="finishSurvey"
-          :disabled="!allQuestionsAnswered || isLoading || isSubmitted"
-          :class="[
-            'w-[152px] h-[56px] rounded-full text-center text-2xl font-bold leading-9',
-            !allQuestionsAnswered || isLoading || isSubmitted
-              ? 'bg-gray-100 text-neutral-300'
-              : 'bg-accent-green text-white'
-          ]"
-        >
-          Publish
-        </button>
-      </div>
-
-      <!-- Alert Box -->
-      <div v-if="!allQuestionsAnswered" class="flex items-center space-x-2 mb-6 text-accent" style="padding-top: 20px;">
-        <inline-svg src="assets/info-icon.svg" class="text-accent w-5 h-5"/>
-        <span class="text-sm font-medium">Complete self review before publishing</span>
       </div>
     </div>
     <ToastView :message="toastMessage" :type="toastType" @hidden="clearToast" />
@@ -178,9 +210,11 @@ export default {
     const creatorAnswers = ref([]);
     const isLoading = ref(false);
     const isSubmitted = ref(false);
+    const isPublished = ref(false);
     const errorMessage = ref('');
     const showSuccess = ref(false);
-    const createdSurveyId = ref(null);
+    const surveyCode = ref('');
+    const surveyLink = ref('');
     const inputFocused = ref(false);
     const toastMessage = ref('');
     const toastType = ref('');
@@ -245,8 +279,10 @@ export default {
         };
         const response = await api.createSurvey(surveyData);
         console.log('Survey created', response.data);
-        createdSurveyId.value = response.data.survey_id;
+        surveyCode.value = response.data.survey_id;
+        surveyLink.value = response.data.share_link;
         isSubmitted.value = true;
+        isPublished.value = true;
         showSuccess.value = true;
         celebrateSuccess();
         toastMessage.value = 'Survey created successfully!';
@@ -256,10 +292,20 @@ export default {
         errorMessage.value = 'Error creating survey. Please try again.';
         toastMessage.value = 'Failed to create survey. Please try again.';
         toastType.value = 'error';
-        isSubmitted.value = false; // Reset isSubmitted on error
       } finally {
         isLoading.value = false;
       }
+    }
+
+    function copyToClipboard(text) {
+      navigator.clipboard.writeText(text).then(() => {
+        toastMessage.value = 'Copied to clipboard!';
+        toastType.value = 'success';
+      }, (err) => {
+        console.error('Could not copy text: ', err);
+        toastMessage.value = 'Failed to copy to clipboard';
+        toastType.value = 'error';
+      });
     }
 
     return {
@@ -270,9 +316,11 @@ export default {
       creatorAnswers,
       isLoading,
       isSubmitted,
+      isPublished,
       errorMessage,
       showSuccess,
-      createdSurveyId,
+      surveyCode,
+      surveyLink,
       allQuestionsAnswered,
       inputFocused,
       isAddButtonDisabled,
@@ -282,7 +330,8 @@ export default {
       addQuestion,
       selectAnswer,
       finishSurvey,
-      clearToast
+      clearToast,
+      copyToClipboard
     };
   }
 }
