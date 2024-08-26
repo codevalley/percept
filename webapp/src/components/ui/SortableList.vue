cb472887d2d3ee7a12015562327c1d39b914da2f
Narayan Babu <codevalley@live.com>
2024-08-26 19:52:34 +0530

    Add a mobile / desktop marker

--- webapp/src/components/ui/SortableList.vue
+++ webapp/src/components/ui/SortableList.vue
@@ -1,24 +1,29 @@
 <template>
-  <draggable 
-    v-if="!isTouchDevice"
-    :model-value="modelValue" 
-    @update:model-value="$emit('update:modelValue', $event)"
-    item-key="id"
-    :disabled="disabled"
-    @start="drag = true"
-    @end="drag = false"
-  >
-    <template #item="{ element }">
-      <div class="sortable-item" :class="{ 'is-dragging': drag }">
+  <div class="sortable-list-container">
+    <draggable 
+      v-if="!isTouchDevice"
+      :model-value="modelValue" 
+      @update:model-value="$emit('update:modelValue', $event)"
+      item-key="id"
+      :disabled="disabled"
+      @start="drag = true"
+      @end="drag = false"
+    >
+      <template #item="{ element }">
+        <div class="sortable-item" :class="{ 'is-dragging': drag }">
+          <slot :item="element"></slot>
+        </div>
+      </template>
+    </draggable>
+
+    <div v-else>
+      <div v-for="element in modelValue" :key="element.id" class="sortable-item">
         <slot :item="element"></slot>
       </div>
-    </template>
-  </draggable>
-
-  <div v-else>
-    <div v-for="element in modelValue" :key="element.id" class="sortable-item">
-      <slot :item="element"></slot>
     </div>
+
+    <!-- Visual marker -->
+    <div class="implementation-marker" :class="{ 'touch': isTouchDevice, 'non-touch': !isTouchDevice }"></div>
   </div>
 </template>
 
@@ -48,6 +53,7 @@ export default {
 
     onMounted(() => {
       isTouchDevice.value = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+      console.log('Is touch device:', isTouchDevice.value);
     });
 
     return {
@@ -59,10 +65,32 @@ export default {
 </script>
 
 <style scoped>
+.sortable-list-container {
+  position: relative;
+}
+
 .sortable-item {
   transition: all 0.3s;
 }
+
 .sortable-item.is-dragging {
   opacity: 0.5;
+}
+
+.implementation-marker {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+}
+
+.implementation-marker.touch {
+  background-color: #ff0000; /* Red for touch devices */
+}
+
+.implementation-marker.non-touch {
+  background-color: #00ff00; /* Green for non-touch devices */
 }
 </style>
